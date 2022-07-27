3d90c71af105f3b51d0805c6965b05583b9e7099
Hans Elde <hanserik.elde@gmail.com>
2022-07-27 09:30:14 -0700

    raw data for routes displayed in table

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+import DATA from './data'
 import './App.css';
 
-const App = () => (
-  <div className="app">
-    <header className="header">
-      <h1 className="title">Airline Routes</h1>
-    </header>
-    <section>
-      <p>
-        Welcome to the app!
-      </p>
-    </section>
-  </div>
-)
+const App = () => {
+  const { routes } = DATA
+  console.log(routes)
+
+  return (
+    <div className="app">
+      <header className="header">
+        <h1 className="title">Airline Routes</h1>
+      </header>
+      <section>
+        <p>
+          Welcome to the app!
+        </p>
+        <table>
+          <tbody>
+            {routes.map((route, index) => {
+              return (
+                <tr key={index}>
+                  <td>{route.airline}</td>
+                  <td>{route.src}</td>
+                  <td>{route.dest}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  )
+}
 
 export default App;
